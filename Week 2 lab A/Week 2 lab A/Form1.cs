b56c8cca18cb0607f8b9e0ba2b39a5713899e86c
KerryParadise <41308826+KerryParadise@users.noreply.github.com>
2018-07-26 08:30:31 -0400

    Add files via upload

---
+++ Week 2 lab A/Week 2 lab A/Form1.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Week_2_lab_A
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        string g1, g2, g3, g4, g5, gAvg, gLab1Avg, gLab2Avg, gLab3Avg, gLab4Avg, gLab5Avg;
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        Int32 intG1, intG2, intG3, intG4, intG5, intGAvg, intLab1Avg, intLab2Avg, intLab3Avg, intLab4Avg, intLab5Avg;
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Console.log(" ")
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
